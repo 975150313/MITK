b544c442bc85d1bc0329a55901f9e31bc61ac7ae
Daniel Maleike <d.maleike@mint-medical.de>
2013-08-09 09:32:08 +0200

    Add some explanations

--- Examples/QtRenderingPerformanceDemonstrator/QmitkRenderScene.h
+++ Examples/QtRenderingPerformanceDemonstrator/QmitkRenderScene.h
@@ -35,7 +35,51 @@ namespace mitk
 class QmitkRenderWindow;
 
 
+/**
+  \brief Rendering scene demonstrator.
 
+  This widget is meant to test rendering performance of QmitkRenderWindow.
+  The user is able to select a desired frame rate, load data for visualization,
+  and get feedback on the achieved frame rate.
+
+  \section s1 Loading data
+
+  The "Load" buttons, including the presets below "Load data..." will
+  all setup a rendering scene, i.e.:
+   - create a new DataStorage, fill it with selected data
+   - setup a QmitkRenderWindow to display this data
+     - initializing a world geometry as RenderingManager would usually do (slices that match DataStorage contents)
+     - using a new RenderingManager for just this render window
+
+  Three preset buttons are meant to quickly load data from the MITK-Data repository,
+  which is checked out in a super-build setup, thus found/described automatically by CMake.
+
+  Preset 1: 2D DICOM image (512x512), some PlanarFigure objects representing measurements on top
+  Preset 2: as preset 1, BUT the image is displayed as a plane in a 3D scene (which currently requires a hidded second render window to be setup)
+  Preset 3: loads a colored 2D image (640x480) in a 2D window
+
+  To get a different setup with a 2D plane in 3D space, it is easiest to exchange data in the MITK-Data repository checkout...
+
+  \section s2 Rendering
+
+  Rendering is triggered by a QTimer. Each timeout() signal leads to a forced rendering update.
+  Elapsed time and rendered frames are both counted and reported to evaluate performance.
+
+  A number of checkboxes are evaluated during loading and rendering to demonstrate several effects:
+   - Scroll each frame: forwards the stepper of the associated SliceNavigationController, i.e. scrolls
+     - this forces rendering to resample something new
+   - Modify data each frame: marks all data modified (by calling Modified()) each frame
+     - this forces rendering to do everything all over, simulating live data
+   - Write text: enables display of PlanarFigure names, which have influence on the rendering time
+   - Contours: decide between binary mask or contour rendering for binary images (contours being generally slower)
+
+   \section s3 Class status
+
+   This is just meant for demonstration of rendering, not for demonstration of nice code style,
+   good structure or recommended use. Some parts are documented, some copied from other places,
+   it is work in progress.
+
+*/
 class QmitkRenderScene : public QWidget
 {
   Q_OBJECT
